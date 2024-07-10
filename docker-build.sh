29587e0b6f1dd8c67faea524bea04265aab83a7f
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 15:35:44 -0300

    echos

--- docker-build.sh
+++ docker-build.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Iniciar sesión en Docker Hub de manera no interactiva
-echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
-
-# Verificar si el inicio de sesión fue exitoso
-if [ $? -ne 0 ]; then
-    echo "Error: Docker login failed."
-    exit 1
-fi
-
+echo "LA CONTRASEÑA ES ${DOCKER_PASSWORD}"
+echo "LA CUENTA ES ${DOCKER_USERNAME}"
+echo "LA CUENTA ES ${DOCKER_ACCOUNT}"
