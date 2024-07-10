199acea63f326508923ba7796590a9336d9515ed
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 15:29:56 -0300

    var good

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
 # Iniciar sesión en Docker Hub de manera no interactiva
-echo "${DOCKER_PASSWORD}"
-echo "${VALEN_VAR}"
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
+
+# Verificar si el inicio de sesión fue exitoso
+if [ $? -ne 0 ]; then
+    echo "Error: Docker login failed."
+    exit 1
+fi
+
+# Construir y empujar las imágenes Docker con las etiquetas correctas
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:apm" apache
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:apm"
+
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:apm" microservice-kubernetes-demo-catalog
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:apm"
+
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:apm" microservice-kubernetes-demo-customer
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:apm"
+
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:apm" microservice-kubernetes-demo-order
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:apm"
