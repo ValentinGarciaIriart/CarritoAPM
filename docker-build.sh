e5fb5ad5ef3397951701dde11a9247a450e98bd1
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 15:18:05 -0300

    secretos?

--- docker-build.sh
+++ docker-build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+# Iniciar sesión en Docker Hub de manera no interactiva
+echo "$(DOCKER_PASSWORD)" | docker login --username "$(DOCKER_USERNAME)" --password-stdin
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm apache
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm
+# Construir y empujar las imágenes Docker con las etiquetas correctas
+docker build --tag $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-apache:apm apache
+docker push $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-apache:apm
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm microservice-kubernetes-demo-catalog
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm
+docker build --tag $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-catalog:apm microservice-kubernetes-demo-catalog
+docker push $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-catalog:apm
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm microservice-kubernetes-demo-customer
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm
+docker build --tag $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-customer:apm microservice-kubernetes-demo-customer
+docker push $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-customer:apm
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm microservice-kubernetes-demo-order
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm
+docker build --tag $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-order:apm microservice-kubernetes-demo-order
+docker push $(DOCKER_ACCOUNT)/microservice-kubernetes-demo-order:apm
