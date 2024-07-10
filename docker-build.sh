82e57b00a72be49e737a4e7b0e48de759527d5d8
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 15:47:03 -0300

    dockerbuild final

--- docker-build.sh
+++ docker-build.sh
@@ -1,27 +1,12 @@
 #!/bin/sh
 
-# Verificar que se han pasado los argumentos esperados
-if [ $# -ne 3 ]; then
-    echo "Uso: $0 <DOCKER_PASSWORD> <DOCKER_USERNAME> <DOCKER_ACCOUNT>"
-    exit 1
-fi
-
 DOCKER_PASSWORD=$1
 DOCKER_USERNAME=$2
 DOCKER_ACCOUNT=$3
 
-echo "LA CONTRASEÑA ES ${DOCKER_PASSWORD}"
-echo "EL USUARIO ES ${DOCKER_USERNAME}"
-echo "LA CUENTA ES ${DOCKER_ACCOUNT}"
-
 # Iniciar sesión en Docker Hub de manera no interactiva
 echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --password-stdin
 
-# Verificar si el inicio de sesión fue exitoso
-if [ $? -ne 0 ]; then
-    echo "Error: Docker login falló."
-    exit 1
-fi
 
 # Construir y empujar las imágenes Docker con las etiquetas correctas
 docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:apm" apache
