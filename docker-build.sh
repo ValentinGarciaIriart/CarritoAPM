0334dacba1ead3f31a2e4f78cfb842b5ba397f25
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 15:38:36 -0300

    ?¡

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Verificar que las variables de entorno estén definidas
+if [ -z "${DOCKER_PASSWORD}" ] || [ -z "${DOCKER_USERNAME}" ] || [ -z "${DOCKER_ACCOUNT}" ]; then
+    echo "Error: Variables de entorno no definidas."
+    exit 1
+fi
+
 echo "LA CONTRASEÑA ES ${DOCKER_PASSWORD}"
-echo "LA CUENTA ES ${DOCKER_USERNAME}"
+echo "EL USUARIO ES ${DOCKER_USERNAME}"
 echo "LA CUENTA ES ${DOCKER_ACCOUNT}"
