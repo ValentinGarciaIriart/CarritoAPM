e54e18ce2d0dfec40c5fe883aad78679fdddf757
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 13:56:12 -0300

    First Commit

---
+++ docker-build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DOCKER_ACCOUNT=valentingarciairiart
+DOCKER_USERNAME=valentingarciairiart
+DOCKER_PASSWORD=Vale0811!
+
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+
+docker build --tag=microservice-kubernetes-demo-apache apache
+docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm
+
+docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
+docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm
+
+docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
+docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm
+
+docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
+docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm
