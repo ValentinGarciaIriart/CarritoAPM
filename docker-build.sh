28551936e7f228ffb9b55e4ee5d7035d8768efd7
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 15:00:17 -0300

    new dockerbuild

--- docker-build.sh
+++ docker-build.sh
@@ -11,16 +11,16 @@ echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdi
 
 docker build --tag=microservice-kubernetes-demo-apache apache
 docker tag microservice-kubernetes-demo-apache $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache
 
 docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
 docker tag microservice-kubernetes-demo-catalog $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog
 
 docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
 docker tag microservice-kubernetes-demo-customer $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer
 
 docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
 docker tag microservice-kubernetes-demo-order $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm
+docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order
