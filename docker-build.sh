79b85722aa53b0ea263e45510f8774a3c547848a
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-10 15:05:43 -0300

    dckbui

--- docker-build.sh
+++ docker-build.sh
@@ -6,16 +6,24 @@ if [ -f .env ]; then
 fi
 set +o allexport
 
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+# Realizar el inicio de sesión en Docker Hub de manera no interactiva
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm apache
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm
+# Verificar si el inicio de sesión fue exitoso
+if [ $? -ne 0 ]; then
+    echo "Error: Docker login failed."
+    exit 1
+fi
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm microservice-kubernetes-demo-catalog
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm
+# Construir y etiquetar las imágenes Docker
+docker build --tag "$DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm" apache
+docker push "$DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:apm"
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm microservice-kubernetes-demo-customer
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm
+docker build --tag "$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm" microservice-kubernetes-demo-catalog
+docker push "$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:apm"
 
-docker build --tag=$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm microservice-kubernetes-demo-order
-docker push $DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm
+docker build --tag "$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm" microservice-kubernetes-demo-customer
+docker push "$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:apm"
+
+docker build --tag "$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm" microservice-kubernetes-demo-order
+docker push "$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:apm"
