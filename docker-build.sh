35fb636613f5bed96c613dcb73db14b57c9a7522
ValentinGarciaIriart <valentingarciairiart@gmail.com>
2024-07-11 12:01:28 -0300

    apm2

--- docker-build.sh
+++ docker-build.sh
@@ -9,14 +9,14 @@ echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_USERNAME}" --passw
 
 
 # Construir y empujar las imágenes Docker con las etiquetas correctas
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:apm" apache
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:apm"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:apm2" apache
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-apache:apm2"
 
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:apm" microservice-kubernetes-demo-catalog
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:apm"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:apm2" microservice-kubernetes-demo-catalog
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-catalog:apm2"
 
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:apm" microservice-kubernetes-demo-customer
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:apm"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:apm2" microservice-kubernetes-demo-customer
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-customer:apm2"
 
-docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:apm" microservice-kubernetes-demo-order
-docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:apm"
+docker build --tag "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:apm2" microservice-kubernetes-demo-order
+docker push "${DOCKER_ACCOUNT}/microservice-kubernetes-demo-order:apm2"
